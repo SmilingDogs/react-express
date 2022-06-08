40411d43aa8c5fafb0589600785e898579ed772e
Oleksiy Kudryashov <o.kudryashov@astoundcommerce.com>
2022-06-08 21:52:55 +0300

    Added Customer Model

---
+++ models/customer.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Customer', customerSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
+const Customer = require('../models/customer');
 
-let customers = [
-    { id: uuidv4(), firstName: 'Jane', lastName: 'Smith' },
-    { id: uuidv4(), firstName: 'Johh', lastName: 'Rambo' },
-    { id: uuidv4(), firstName: 'Chuck', lastName: 'Norris'}
-  ];
+// let customers = [
+//     { id: uuidv4(), firstName: 'Jason', lastName: 'Statham' },
+//     { id: uuidv4(), firstName: 'Arnold', lastName: 'Schwarzenegger' },
+//     { id: uuidv4(), firstName: 'Jean-Claude', lastName: 'Van Damme'}
+//   ];
 
 router.get('/', (req, res) => {
   res.send(`<h1>Hello World</h1>`);
 });
 
+//Getting all customers
+
 router.get('/api/customers', (req, res) => {
-  res.send(customers);
+  // res.send(customers);
+  Customer.find()
+    .then((data) => {
+      const customers = data;
+      res.send(customers);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
 });
+
+//Getting certain customer by id
 
 router.get('/api/customers/:id', (req, res) => {
   const { id } = req.params;
@@ -27,6 +40,37 @@ router.get('/api/customers/:id', (req, res) => {
 
   res.send(customer); //* отправить отдельного customerа
 });
+//Creating new customer
+
+router.post('/api/customers', (req, res) => {
+  const { error } = validateCustomer(req.body);
+
+  if (error) return res.status(400).send(error.message);
+
+  // const newCustomer = {
+  //   id: uuidv4(), //*normally id is assigned by Mongo or other DBs automatically
+  //   firstName: req.body.firstName,
+  //   lastName: req.body.lastName,
+  // };
+  // customers.push(newCustomer);
+  // res.status(201).send(newCustomer);
+
+  const customer = new Customer({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  });
+  customer
+    .save()
+    .then(() => {
+      const newCustomer = customer;
+      res.status(201).send(newCustomer);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.message });
+    });
+});
+
+//Updating certain customer
 
 router.put('/api/customers/:id', (req, res) => {
   //* Look up the customer
@@ -49,25 +93,16 @@ router.put('/api/customers/:id', (req, res) => {
   res.send(customer); //* then return the updated customer to the client
 });
 
-router.post('/api/customers', (req, res) => {
-  const { error } = validateCustomer(req.body);
-
-  if (error) return res.status(400).send(error.message);
-
-  const newCustomer = {
-    id: uuidv4(), //*normally id is assigned by Mongo or other DBs automatically
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-  };
-  customers.push(newCustomer);
-  res.status(201).send(newCustomer);
-});
+//Deleting exsiting customer
 
 router.delete('/api/customers/:id', (req, res) => {
   const { id } = req.params;
+
   const customerToDelete = customers.find((c) => c.id === id);
-  if (!customerToDelete)
+
+  if (!customerToDelete) {
     return res.status(404).send('The customer with this ID was not found...');
+  }
 
   const deleteIndex = customers.indexOf(customerToDelete);
   customers.splice(deleteIndex, 1);
@@ -75,14 +110,14 @@ router.delete('/api/customers/:id', (req, res) => {
 });
 
 function validateCustomer(customer) {
-    //* выносим Валидацию Joi отдельно чтобы не дублировать в разных местах.
-  
-    const schema = Joi.object({
-      firstName: Joi.string().required(),
-      lastName: Joi.string().required(),
-    });
-  
-    return schema.validate(customer);
-  }
+  //* выносим Валидацию Joi отдельно чтобы не дублировать в разных местах.
+
+  const schema = Joi.object({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+  });
+
+  return schema.validate(customer);
+}
 
 module.exports = router;
